2cc4dbdc0c3af2ab884bef4757c2011c24906698
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-09-01 20:40:06 +0900

    (2025.09.01) TestApp 추가

---
+++ TestApp/TestApp/TestApp.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+bool is_leap(int y)
+{
+	bool answer = false;
+
+	if (y % 400 == 0)
+		answer = true;
+
+	else if (y % 4 == 0)
+	{
+		answer = true;
+
+		if (y % 100 == 0)
+			answer = false;
+	}
+
+
+	return answer;
+}
+
+int main()
+{
+	cout << boolalpha << is_leap(2000) << " " << is_leap(1900) << " " << is_leap(2024) << "\n"; // true false true
+}
